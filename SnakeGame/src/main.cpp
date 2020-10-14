094692665803a365c1731c207f1c14f1f2c0da81
Duwles <duwless@outlook.com>
2020-10-14 12:54:54 +0200

    added

---
+++ SnakeGame/src/main.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    system("cls");
+    SetConsoleTitleW(L"cApp - Snake Game");
+
+    cout << "Snake Game" << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/SnakeGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeGame.dir/SnakeGame/src/main.cpp.obj"
+  "SnakeGame.exe"
+  "SnakeGame.exe.manifest"
+  "SnakeGame.lib"
+  "SnakeGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
